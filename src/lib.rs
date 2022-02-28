1e524f38c0c1a6d95a8299099ffb2603261076ea
Nell Shamrell <nellshamrell@gmail.com>
2022-02-28 12:21:24 -0800

    initial refactor
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,47 +5,59 @@ use alloc::format;
 use alloc::string::{String, ToString};
 use std::io::{BufReader, Read};
 
-pub fn generate_sha1_git_oid(x: &[u8]) -> String {
-    let prefix = format!("blob {}\0", x.len());
-    let mut hasher = sha1_smol::Sha1::new();
-
-    hasher.update(prefix.as_bytes());
-    hasher.update(x);
-
-    hasher.digest().to_string()
+enum HashAlgorithm {
+    SHA1,
+    SHA2
 }
 
-pub fn generate_sha1_git_oid_from_buffer<R>(
-    mut reader: BufReader<R>,
-    expected_length: usize,
-) -> String
-where
-    BufReader<R>: std::io::Read,
-{
-    let prefix = format!("blob {}\0", expected_length);
-    let mut hasher = sha1_smol::Sha1::new();
+struct GitOid {
+    hash_algorithm: HashAlgorithm,
+}
 
-    hasher.update(prefix.as_bytes());
+impl GitOid {
+    pub fn generate_sha1_git_oid(&self, x: &[u8]) -> String {
+        let prefix = format!("blob {}\0", x.len());
+        let mut hasher = sha1_smol::Sha1::new();
 
-    let mut buf = [0; 4096]; // linux default page size is 4096
-    let mut amount_read = 0;
-    loop {
-        let y = reader.read(&mut buf);
-        match y {
-            Ok(0) => {
-                break;
-            }
-            Ok(size) => {
-                hasher.update(&buf[..size]);
-                amount_read = amount_read + size;
-            }
-            Err(_) => {
-                break;
-            }
-        }
+        hasher.update(prefix.as_bytes());
+        hasher.update(x);
+
+        hasher.digest().to_string()
     }
 
-    hasher.digest().to_string()
+    pub fn generate_sha1_git_oid_from_buffer<R>(
+        &self,
+        mut reader: BufReader<R>,
+        expected_length: usize,
+    ) -> String
+    where
+        BufReader<R>: std::io::Read,
+    {
+        let prefix = format!("blob {}\0", expected_length);
+        let mut hasher = sha1_smol::Sha1::new();
+
+        hasher.update(prefix.as_bytes());
+
+        let mut buf = [0; 4096]; // linux default page size is 4096
+        let mut amount_read = 0;
+        loop {
+            let y = reader.read(&mut buf);
+            match y {
+                Ok(0) => {
+                    break;
+                }
+                Ok(size) => {
+                    hasher.update(&buf[..size]);
+                    amount_read = amount_read + size;
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+
+        hasher.digest().to_string()
+    }
 }
 
 #[cfg(test)]
@@ -53,8 +65,7 @@ mod tests {
     use std::fs::File;
     use std::io::BufReader;
 
-    use crate::generate_sha1_git_oid;
-    use crate::generate_sha1_git_oid_from_buffer;
+    use super::*;
 
     #[test]
     fn it_works() {
@@ -66,7 +77,11 @@ mod tests {
     fn test_generate_sha1_git_oid() {
         let input = "hello world".as_bytes();
 
-        let result = generate_sha1_git_oid(input);
+        let new_gitoid = GitOid {
+            hash_algorithm: HashAlgorithm::SHA1
+        };
+
+        let result = new_gitoid.generate_sha1_git_oid(input);
         assert_eq!(result, "95d09f2b10159347eece71399a7e2e907ea3df4f")
     }
 
@@ -77,9 +92,13 @@ mod tests {
             Ok(f) => {
                 let reader = BufReader::new(f);
 
-                let hash = generate_sha1_git_oid_from_buffer(reader, 11);
+                let new_gitoid = GitOid {
+                    hash_algorithm: HashAlgorithm::SHA1
+                };
 
-                assert_eq!("95d09f2b10159347eece71399a7e2e907ea3df4f", hash)
+                let result = new_gitoid.generate_sha1_git_oid_from_buffer(reader, 11);
+
+                assert_eq!("95d09f2b10159347eece71399a7e2e907ea3df4f", result)
             }
             Err(_) => {
                 assert!(false)
