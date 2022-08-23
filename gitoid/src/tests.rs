3b89cc026ee2c6c57b9f8ad7076c265455516b8a
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2022-08-23 16:11:59 -0700

    Improve consistency of gitoid tests.
    This commit updates the `tests` module in the `gitoid` crate to
    make sure that tests between SHA1 and SHA256 and between byte
    inputs and buffer inputs are consistent. It also moves tests which
    previously used `unwrap` to instead use `?`, with the intent of
    reducing line noise.
    
    Note that some tests now include more than one `assert_eq`, to
    ensure both that the hashing is correct (this is really just
    checking that the correct hashing method is called --- we assume
    that the dependencies used for hashing implement the hash functions
    correctly), and that the construction of the `gitoid` string works
    as-intended.
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- gitoid/src/tests.rs
+++ gitoid/src/tests.rs
@@ -1,76 +1,118 @@
 use super::*;
+use hash_algorithm::HashAlgorithm::*;
+use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 
-#[test]
-fn test_generate_sha1_git_oid() {
-    let input = "hello world".as_bytes();
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-    let result = GitOid::new(HashAlgorithm::SHA1, input);
+#[test]
+fn generate_sha1_gitoid_from_bytes() {
+    let input = "hello world".as_bytes();
+    let result = GitOid::new(SHA1, input);
+
+    assert_eq!(
+        result.hex_hash(),
+        "95d09f2b10159347eece71399a7e2e907ea3df4f"
+    );
 
     assert_eq!(
         result.to_string(),
         "SHA1:95d09f2b10159347eece71399a7e2e907ea3df4f"
-    )
-}
-
-#[test]
-fn test_generate_sha1_git_oid_buffer() {
-    let file = File::open("test/data/hello_world.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let result = GitOid::new_from_reader(HashAlgorithm::SHA1, reader, 11).unwrap();
-
-    assert_eq!(
-        "95d09f2b10159347eece71399a7e2e907ea3df4f",
-        result.hex_hash()
-    )
-}
-
-#[test]
-fn test_generate_sha256_git_oid() {
-    let input = "hello world".as_bytes();
-
-    let result = GitOid::new(HashAlgorithm::SHA256, input);
-
-    assert_eq!(
-        "fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03",
-        result.hex_hash()
     );
 }
 
 #[test]
-fn test_generate_sha256_git_oid_buffer() {
-    let file = File::open("test/data/hello_world.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let result = GitOid::new_from_reader(HashAlgorithm::SHA256, reader, 11).unwrap();
+fn generate_sha1_gitoid_from_buffer() -> Result<()> {
+    let reader = BufReader::new(File::open("test/data/hello_world.txt")?);
+    let result = GitOid::new_from_reader(SHA1, reader, 11)?;
 
     assert_eq!(
-        "fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03",
-        result.hex_hash()
+        result.hex_hash(),
+        "95d09f2b10159347eece71399a7e2e907ea3df4f"
     );
+
+    assert_eq!(
+        result.to_string(),
+        "SHA1:95d09f2b10159347eece71399a7e2e907ea3df4f"
+    );
+
+    Ok(())
 }
 
 #[tokio::test]
-async fn test_async_read() {
+async fn generate_sha1_gitoids_from_async_buffers() -> Result<()> {
     let mut to_read = Vec::new();
     for _ in 0..50 {
         to_read.push(Source::new(
-            tokio::fs::File::open("test/data/hello_world.txt")
-                .await
-                .unwrap(),
+            tokio::fs::File::open("test/data/hello_world.txt").await?,
             11,
         ));
     }
 
-    let res = GitOid::new_from_async_readers(HashAlgorithm::SHA256, to_read)
-        .await
-        .unwrap();
+    let res = GitOid::new_from_async_readers(SHA1, to_read).await?;
+
+    assert_eq!(50, res.len());
+    assert_eq!(
+        "SHA1:95d09f2b10159347eece71399a7e2e907ea3df4f",
+        res[0].to_string()
+    );
+
+    Ok(())
+}
+
+
+#[test]
+fn generate_sha256_gitoid_from_bytes() {
+    let input = "hello world".as_bytes();
+    let result = GitOid::new(SHA256, input);
+
+    assert_eq!(
+        result.hex_hash(),
+        "fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03"
+    );
+
+    assert_eq!(
+        result.to_string(),
+        "SHA256:fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03"
+    );
+}
+
+#[test]
+fn generate_sha256_gitoid_from_buffer() -> Result<()> {
+    let reader = BufReader::new(File::open("test/data/hello_world.txt")?);
+    let result = GitOid::new_from_reader(SHA256, reader, 11)?;
+
+    assert_eq!(
+        result.hex_hash(),
+        "fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03"
+    );
+
+    assert_eq!(
+        result.to_string(),
+        "SHA256:fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03"
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn generate_sha256_gitoids_from_async_buffers() -> Result<()> {
+    let mut to_read = Vec::new();
+    for _ in 0..50 {
+        to_read.push(Source::new(
+            tokio::fs::File::open("test/data/hello_world.txt").await?,
+            11,
+        ));
+    }
+
+    let res = GitOid::new_from_async_readers(SHA256, to_read).await?;
 
     assert_eq!(50, res.len());
     assert_eq!(
         "SHA256:fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03",
         res[0].to_string()
     );
+
+    Ok(())
 }
