2b310c19300d70ef463dbf864ee858001defdbe0
Nell Shamrell <nellshamrell@gmail.com>
2022-11-01 16:50:49 -0700

    attempts to add c compatible new_from_str function for GitOid struct
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- gitoid/src/ffi/gitoid.rs
+++ gitoid/src/ffi/gitoid.rs
@@ -2,6 +2,7 @@ use crate::HashAlgorithm;
 use crate::ObjectType;
 use crate::GitOid;
 use std::slice;
+use std::ffi::CStr;
 
 #[no_mangle]
 pub extern fn new_from_bytes(
@@ -13,4 +14,21 @@ pub extern fn new_from_bytes(
     // TODO: Make sure that content_len is less than or equal to isize::MAX.
     let content = unsafe { slice::from_raw_parts(content, content_len) };
     GitOid::new_from_bytes(hash_algorithm, object_type, content)
+}
+
+#[no_mangle]
+pub extern fn new_from_str(
+    hash_algorithm: HashAlgorithm,
+    object_type: ObjectType,
+    s: *const c_char,
+) -> GitOid {
+    // Based off https://stackoverflow.com/a/24148033/2308264
+
+    // TODO: Also make sure that content_len is less than or equal to isize::MAX.
+    // CStr::from_ptr calls slice::from_raw_parts
+    // Are there other safety checks we should do here?
+    // https://doc.rust-lang.org/std/ffi/struct.CStr.html#method.from_ptr
+    let content_string: &CStr = unsafe { CStr::from_ptr(s) };
+    let content = content_string.as_bytes();
+    GitOid::new_from_str(hash_algorithm, object_type, content)
 }
