91b8ea8943f518c2855adf3b9cb59d44066fbab9
Nell Shamrell <nellshamrell@gmail.com>
2022-11-09 15:36:47 -0800

    adds tests and instructions for test for C bindings
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- gitoid/src/ffi/gitoid.rs
+++ gitoid/src/ffi/gitoid.rs
@@ -12,6 +12,7 @@ pub extern fn new_from_bytes(
     content: *const u8,
     content_len: usize,
 ) -> GitOid {
+
     // TODO: Make sure that content_len is less than or equal to isize::MAX.
     let content = unsafe { slice::from_raw_parts(content, content_len) };
     GitOid::new_from_bytes(hash_algorithm, object_type, content)
