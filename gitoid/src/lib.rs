0b64c90a0d5661d2183ab961f82e2f0b17781991
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2022-12-05 08:48:35 -0800

    Implemented hash getters for FFI
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- gitoid/src/lib.rs
+++ gitoid/src/lib.rs
@@ -41,7 +41,7 @@ mod builder;
 mod error;
 mod ffi;
 mod gitoid;
-mod hash;
+mod hash_ref;
 mod hash_algorithm;
 mod object_type;
 #[cfg(test)]
@@ -50,6 +50,6 @@ mod tests;
 pub use crate::builder::*;
 pub use crate::error::*;
 pub use crate::gitoid::*;
-pub use crate::hash::*;
+pub use crate::hash_ref::*;
 pub use crate::hash_algorithm::*;
 pub use crate::object_type::*;
