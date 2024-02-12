db3344c8ece855144fc5f7660ccf41b639cbd302
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-02-12 21:31:47 +0000

    Implement ArtifactID type.
    This initial commit just defines an artifact ID type that selects
    the specific construct of the GitOid type.
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- omnibor/src/lib.rs
+++ omnibor/src/lib.rs
@@ -1,1 +1,8 @@
-// TODO(abrinker): Complete first draft of the rewrite of this crate.
+//! OmniBOR in Rust.
+
+use gitoid::hash::Sha256;
+use gitoid::object::Blob;
+use gitoid::GitOid;
+
+/// An OmniBOR Artifact Identifier.
+pub type ArtifactId = GitOid<Sha256, Blob>;
