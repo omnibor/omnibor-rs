739f9971643a4fd17b11a746d1a2ee6f0f697f4b
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-02-24 01:03:23 +0000

    fix: Fix broken rollback in xtask pipeline.
    The prior implementation of step pipelining in xtask actually didn't
    implement rollback correctly, and would "undo" steps it hadn't done
    yet. This new implementation is correct.
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- xtask/src/release.rs
+++ xtask/src/release.rs
@@ -1,33 +1,40 @@
+use crate::cli::{Bump, Crate};
 use crate::pipeline::{self, Step};
 use crate::step;
-use crate::cli::{Crate, Bump};
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context as _, Result};
 use clap::ArgMatches;
 
 /*
-    # Run `git-cliff` to generate a changelog.
-    # Commit the changelog w/ commit msg in Conventional Commit fmt.
-    # Run `cargo-release` to release the new version.
-    # If anything fails, rollback prior steps in reverse order.
-    # Probably good for each step to have a "do" and "undo" operation.
-    #
-    # ... In fact I'll probably write this in Rust lol.
+   # Run `git-cliff` to generate a changelog.
+   # Commit the changelog w/ commit msg in Conventional Commit fmt.
+   # Run `cargo-release` to release the new version.
+   # If anything fails, rollback prior steps in reverse order.
+   # Probably good for each step to have a "do" and "undo" operation.
+   #
+   # ... In fact I'll probably write this in Rust lol.
 
-    # Need:
-    #
-    # - git
-    # - git-cliff
-    # - cargo
-    # - cargo-release
- */
-
+   # Need:
+   #
+   # - git
+   # - git-cliff
+   # - cargo
+   # - cargo-release
+*/
 
 /// Run the release command.
 pub fn run(args: &ArgMatches) -> Result<()> {
-    let krate: Crate = *args.get_one("crate").ok_or_else(|| anyhow!("'--crate' is a required argument"))?;
-    let bump: Bump = *args.get_one("bump").ok_or_else(|| anyhow!("'--bump' is a required argument"))?;
+    let krate: Crate = *args
+        .get_one("crate")
+        .ok_or_else(|| anyhow!("'--crate' is a required argument"))?;
+    let bump: Bump = *args
+        .get_one("bump")
+        .ok_or_else(|| anyhow!("'--bump' is a required argument"))?;
 
-    log::info!("running 'release', bumping the {} number for crate '{}'", bump, krate);
+    log::info!(
+        "running 'release', bumping the {} number for crate '{}'",
+        bump,
+        krate
+    );
 
     pipeline::run([
         step!(CheckDependencies),
@@ -45,11 +52,19 @@ impl Step for CheckDependencies {
     }
 
     fn run(&mut self) -> Result<()> {
+        check_cmd("git")?;
+        check_cmd("git-cliff")?;
+        check_cmd("cargo")?;
+        check_cmd("cargo-release")?;
         Ok(())
     }
 
     fn undo(&mut self) -> Result<()> {
         Ok(())
+    }
+
+    fn can_skip_undo(&self) -> bool {
+        true
     }
 }
 
@@ -99,4 +114,11 @@ impl Step for ReleaseCrate {
     fn undo(&mut self) -> Result<()> {
         Ok(())
     }
+}
+
+/// Check if a command exists on the command line.
+fn check_cmd(name: &str) -> Result<()> {
+    which::which(name)
+        .map(|_| ())
+        .context(format!("failed to find command '{}'", name))
 }
