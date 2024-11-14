0fc33c293d4171082cf6672cc755c4080979b34c
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-11-14 09:37:47 -0800

    feat: Added snapshot tests for `omnibor artifact id` (#213)
    This adds snapshot tests for all three formats of the `omnibor artifact id`
    subcommand, to catch formatting regressions in the future.
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- omnibor-cli/tests/test.rs
+++ omnibor-cli/tests/test.rs
@@ -6,6 +6,7 @@ macro_rules! settings {
     ($block:expr) => {
         let mut settings = Settings::clone_current();
         settings.add_filter(r#"omnibor(?:\.exe)?"#, "omnibor");
+        settings.add_filter(r#"gitoid:blob:sha256:[a-f0-9]{64}"#, "<GITOID>");
         settings.bind(|| $block);
     };
 }
@@ -35,5 +36,47 @@ fn manifest_no_args() {
 fn debug_no_args() {
     settings!({
         assert_cmd_snapshot!(Command::new(get_cargo_bin("omnibor")).arg("debug"));
+    });
+}
+
+#[test]
+fn artifact_id_plain() {
+    settings!({
+        assert_cmd_snapshot!(Command::new(get_cargo_bin("omnibor")).args([
+            "artifact",
+            "id",
+            "--format",
+            "plain",
+            "--path",
+            "tests/data/main.c"
+        ]))
+    });
+}
+
+#[test]
+fn artifact_id_short() {
+    settings!({
+        assert_cmd_snapshot!(Command::new(get_cargo_bin("omnibor")).args([
+            "artifact",
+            "id",
+            "--format",
+            "short",
+            "--path",
+            "tests/data/main.c"
+        ]))
+    });
+}
+
+#[test]
+fn artifact_id_json() {
+    settings!({
+        assert_cmd_snapshot!(Command::new(get_cargo_bin("omnibor")).args([
+            "artifact",
+            "id",
+            "--format",
+            "json",
+            "--path",
+            "tests/data/main.c"
+        ]))
     });
 }
